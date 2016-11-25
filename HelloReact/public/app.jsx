efc3bb21b07049350c8c5b774ccb857a119fd801
rohitiyengar <rohit.iyengar@gmail.com>
2016-11-24 22:44:11 -0600

    Back to the original

--- HelloReact/public/app.jsx
+++ HelloReact/public/app.jsx
@@ -1,15 +1,16 @@
 var Greeter = React.createClass({
   render: function(){
-    return React.createElement(
-        'h1',
-        null,
-        'Hello Fun'
-      )
-      // <div>
-      //   <h1>Hello World!</h1>
-      //   <p>This is from a component</p>
-      // </div>
-
+    return (
+    //React.createElement(
+        //'h1',
+        //null,
+        //'Hello Fun'
+      //)
+       <div>
+         <h1>Hello World!</h1>
+         <p>This is from a component</p>
+       </div>
+     );
   }
 });
 
